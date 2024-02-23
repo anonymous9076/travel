1ef00d8613f84ba3e573ad72fcd63572a40dcb6c
anonymous9076 <vijaytushar915@gmail.com>
2024-02-24 00:15:55 +0530

    latest

---
+++ location.js
@@ -0,0 +1,47 @@
+const location = [
+  {
+    location: 'Tokyo, Japan',
+    flightPrice: 'Rs 96,000',
+    slogan: 'Discover the blend of tradition and modernity!',
+    image: 'img2.png'
+  },
+  {
+    location: 'Paris, France',
+    flightPrice: 'Rs 64,000',
+    slogan: 'Experience the City of Love!',
+    image: 'img8.png'
+  },
+
+  {
+    location: 'Cape Town, South Africa',
+    flightPrice: 'Rs 60,500',
+    slogan: 'Explore the beauty of the Mother City!',
+    image: 'img4.png'
+  },
+  {
+    location: 'Sydney, Australia',
+    flightPrice: 'Rs 80,000',
+    slogan: 'Dive into the Land Down Under!',
+    image: 'img5.png'
+  },
+  {
+    location: 'Rio de Janeiro, Brazil',
+    flightPrice: 'Rs 50,300',
+    slogan: 'Feel the rhythm of samba in the Marvelous City!',
+    image: 'img6.png'
+  },
+  {
+    location: 'Dubai, UAE',
+    flightPrice: 'Rs 15,000',
+    slogan: 'Experience luxury in the heart of the desert!',
+    image: 'img7.png'
+  },
+  {
+    location: 'Marrakech, Morocco',
+    flightPrice: 'Rs 20,000',
+    slogan: 'Immerse yourself in the colors of the Red City!',
+    image: 'img1.png'
+  }
+
+]
+export default location
